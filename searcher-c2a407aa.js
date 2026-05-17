57080c9b679da8281c2c2caa71600066b772cec1
Tednoob17 <Tednoob17@users.noreply.github.com>
2026-05-17 20:03:35 +0000

    deploy: d779fd25b62e702ef563b52f1f0b0d724a83f2e4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-33f9af1f.js',
+                path_to_root + 'searchindex-1f255e44.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
